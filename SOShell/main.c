5a60f3b1ec1c57096be57b442d9a4ddb7e891317
Walter Santacruz <walter.santacruz@correounivalle.edu.co>
2018-05-12 16:31:49 -0500

    Add files via upload

--- SOShell/main.c
+++ SOShell/main.c
@@ -1,9 +1,7 @@
-#include <stdio.h>                                                               
-#include <stdlib.h>                                                                
+#include <stdio.h>                                                                                                                              
 #include <unistd.h>                                                                
 #include <string.h>                                                                
 #include <sys/wait.h>
-#include <fcntl.h> 
 #include "funciones.h"
 
 
